5ea8d727e80bba5108ca313ac2acc6fe4bd4dba2
Ignacio  Anaya <ianaya@IANAYA-MAC.local>
2015-04-21 00:07:13 -0300

    Adding new updates

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -1,5 +1,13 @@
 var express = require('express'),
   router = express.Router();
+  nodemailer = require('nodemailer');
+  transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'tree.anaya@gmail.com',
+        pass: 'carlos1957'
+    }
+  });
 
 module.exports = function (app) {
   app.use('/contact', router);
@@ -12,5 +20,24 @@ router.get(['/'], function (req, res, next) {
 });
 
 router.post(['/'], function (req, res, next) {
-   res.end('ok');
+  console.log(req.body);
+  if (!isEmailValid(req.body)) throw 'Missing parametters';
+
+  transporter.sendMail({
+    from: req.body.email,
+    to: 'tree.anaya@gmail.com',
+    subject: 'Consulta Sitio Web - ' + req.body.subject,
+    text: 'Nombre: ' + req.body.name + '\n' +  'E-Mail: ' + req.body.email + '\n' + 'Consulta: ' + req.body.message
+  });
+
+  res.send('OK');
 });
+
+function isEmailValid(body) {
+  if (!body.name) return false;
+  if (!body.email) return false;
+  if (!body.subject) return false;
+  if (!body.message) return false;
+
+  return true;
+}
--- public/js/contact.js
+++ public/js/contact.js
@@ -1,6 +1,6 @@
 $(function(){
 
-  $('#btnSubmit').on('click', function(){
+  $('#btnSubmit').on('click', function() {
 
     if (!isFormValid()) return;
 
@@ -8,23 +8,53 @@ $(function(){
 
   });
 
-  function isFormValid(){
+  function isFormValid() {
+    $('.form-control').removeClass('error');
 
+    $('#errorMessage').hide();
+
+    var isValid = true;
+
+    if (!$('#txtName').val()) {
+      $('#txtName').addClass('error');
+      isValid = false;
+    }
+
+    if (!$('#txtEmail').val()) {
+      $('#txtEmail').addClass('error');
+      isValid = false;
+    }
+
+    if (!$('#txtSubject').val()) {
+      $('#txtSubject').addClass('error');
+      isValid = false;
+    }
+
+    if (!$('#txtMessage').val()) {
+      $('#txtMessage').addClass('error');
+      isValid = false;
+    }
+
+    if (!isValid) $('#errorMessage').show();
+    return isValid;
   }
 
-  function submitForm(){
+  function submitForm() {
     $.ajax({
       type: 'POST',
       url: 'contact',
       data: {
-        name: $('txtName').val(),
-        email: $('txtEmail').val(),
-        subject: $('txtSubject').val(),
-        message: $('txtMessage').val(),
+        name: $('#txtName').val(),
+        email: $('#txtEmail').val(),
+        subject: $('#txtSubject').val(),
+        message: $('#txtMessage').val(),
       },
-      success: function(data){
+      success: function(data) {
         $('#succesModal').modal('show');
       },
+      error: function() {
+        alert('Algo ha salido mal, vuelva a intentarlo mas tarde.');
+      }
     });
   }
 
